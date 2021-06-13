16776aac8cdda7826034ea249065695a9bb32de3
sopranoelfuego <aubinjaja@gmail.com>
2021-06-13 09:10:46 +0200

    (user athentification) debbging section ends

--- client/src/components/context/auth.js
+++ client/src/components/context/auth.js
@@ -1,37 +1,33 @@
-import React,{useContext,createContext,useReducer} from 'react'
-import {LOGIN,LOGOUT}from './types.js'
+import React, { useContext, createContext, useReducer } from 'react'
+import { LOGIN, LOGOUT } from './types.js'
 
-const AuthStateContext=createContext()
-const AuthDispatchContext=createContext()
+const AuthStateContext = createContext()
+const AuthDispatchContext = createContext()
 
-
-
-const authReducer=(state,action)=>{
-
-    switch(action.types){
-      case LOGIN :
-        localStorage.setItem('user',JSON.stringify(action.payload))
-          return {...state,user:action.payload}
-      case LOGOUT: return {...state,user:null} 
-      default :return state
-    }
-
-
+const authReducer = (state, action) => {
+ switch (action.type) {
+  case LOGIN:
+   console.log('this is the object set', action.payload)
+   localStorage.setItem('user', JSON.stringify(action.payload))
+   return { ...state, user: action.payload }
+  case LOGOUT:
+   return { ...state, user: null }
+  default:
+   throw new Error()
+ }
 }
 
-export const AuthProvider=({children})=>{
+export const AuthProvider = ({ children }) => {
+ const [state, dispatch] = useReducer(authReducer, { user: null })
 
-
-    const [state,dispatch]=useReducer(authReducer,{user:null})
-
-    return (
-        <AuthDispatchContext.Provider value={dispatch}>
-            <AuthStateContext.Provider value={state}>
-                    {children}
-            </AuthStateContext.Provider>
-        </AuthDispatchContext.Provider>
-    )
+ return (
+  <AuthDispatchContext.Provider value={dispatch}>
+   <AuthStateContext.Provider value={state}>
+    {children}
+   </AuthStateContext.Provider>
+  </AuthDispatchContext.Provider>
+ )
 }
 
-export const useAuthState=()=>useContext(AuthStateContext)
-export const useAuthDispatch=()=>useContext(AuthDispatchContext)
+export const useAuthState = () => useContext(AuthStateContext)
+export const useAuthDispatch = () => useContext(AuthDispatchContext)
--- client/src/pages/signin/Signin.js
+++ client/src/pages/signin/Signin.js
@@ -1,74 +1,82 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {gql, useLazyQuery} from '@apollo/client'
-import {useAuthDispatch} from '../../components/context/auth.js'
-import {LOGIN} from '../../components/context/types.js'
+import { gql, useLazyQuery } from '@apollo/client'
+import { useAuthDispatch } from '../../components/context/auth.js'
+import { LOGIN } from '../../components/context/types.js'
 
-
-const LOGIN_USER=gql`
-   query loginUser($email:String!,$password:String!){
-       login(input:{email:$email,password:$password}){
-           _id
-           username
-           email
-           token
-
-       }
-   }
+const LOGIN_USER = gql`
+ query loginUser($email: String!, $password: String!) {
+  login(input: { email: $email, password: $password }) {
+   _id
+   username
+   email
+   token
+  }
+ }
 `
 function Signin() {
-    
-    const [variables, setvariables] = useState({
-        email:"",
-        password:""
-    })
-    const dispatch=useAuthDispatch()
-    const [errors, setErrors] = useState({})
-  const [registerUser,{loading}]=useLazyQuery(LOGIN_USER,{
-      onError:(err)=>err.graphQLErrors[0] != null ? setErrors(err.graphQLErrors[0].extensions.errors):setErrors({connexion:"connection error.."}),
-      onCompleted:({login})=>{
-          console.log(login)
-        dispatch({type:LOGIN,payload:login})
+ const [variables, setvariables] = useState({
+  email: '',
+  password: '',
+ })
+ const dispatch = useAuthDispatch()
+ const [errors, setErrors] = useState({})
+ const [registerUser, { loading }] = useLazyQuery(LOGIN_USER, {
+  onError: (err) =>
+   err.graphQLErrors[0] != null
+    ? setErrors(err.graphQLErrors[0].extensions.errors)
+    : setErrors({ connexion: 'connection error..' }),
+  onCompleted: ({ login }) => {
+   console.log('from signin page', login)
+   dispatch({ type: LOGIN, payload: login })
+  },
+ })
 
-      }
-  })
+ const handleChange = (e) => {
+  setvariables({ ...variables, [e.target.name]: e.target.value })
+ }
+ const handleSubmit = (e) => {
+  e.preventDefault()
 
-    const handleChange=(e)=>{
-        setvariables({...variables,[e.target.name]:e.target.value})
-    }
-    const handleSubmit=(e)=>{
-        e.preventDefault()
+  registerUser({ variables })
+ }
+ errors?.connexion && <p>check your connexion</p>
 
-        registerUser({variables})
-    }
-    errors?.connexion && <p>check your connexion</p>
-  
-    return (
-        <div className="row p-5">
-            <div className="col-6  bg-secondary">
-                ahsjahsjha
-            </div>
-            <div className="col-6 ">
-
-            <form className="m-5" onSubmit={handleSubmit} >
-                 <div className="form-group">
-                     <label>email</label>
-                    <input className={errors.email ? "form-control is-invalid":"form-control"} type="email" name="email" onChange={handleChange}/>
-                    <small className="text-danger">{errors && errors.email}</small>
-                 </div>
-                 <div className="form-group">
-                     <label>password</label>
-                    <input className={errors.password ? "form-control is-invalid":"form-control"} type="password" name="password" onChange={handleChange}/>
-                    <small className="text-danger">{errors && errors.password}</small>
-                 </div>
-                 <button type="submit" className="btn btn-primary my-2" disabled={loading}>sign</button>
-                 <div>
-                     you are new create a account? <Link to="/register">register now</Link>
-                 </div>
-             </form>
-            </div>
-        </div>
-    )
+ return (
+  <div className="row p-5">
+   <div className="col-6  bg-secondary">ahsjahsjha</div>
+   <div className="col-6 ">
+    <form className="m-5" onSubmit={handleSubmit}>
+     <div className="form-group">
+      <label>email</label>
+      <input
+       className={errors.email ? 'form-control is-invalid' : 'form-control'}
+       type="email"
+       name="email"
+       onChange={handleChange}
+      />
+      <small className="text-danger">{errors && errors.email}</small>
+     </div>
+     <div className="form-group">
+      <label>password</label>
+      <input
+       className={errors.password ? 'form-control is-invalid' : 'form-control'}
+       type="password"
+       name="password"
+       onChange={handleChange}
+      />
+      <small className="text-danger">{errors && errors.password}</small>
+     </div>
+     <button type="submit" className="btn btn-primary my-2" disabled={loading}>
+      sign
+     </button>
+     <div>
+      you are new create a account? <Link to="/register">register now</Link>
+     </div>
+    </form>
+   </div>
+  </div>
+ )
 }
 
 export default Signin
